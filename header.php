73293dbc2af3451b3607b9429118faa01d1f2764
Tomasz Banasiak <tomasz.banasiak@rst.com.pl>
2026-03-03 21:01:21 +0100

    Fixes in header

--- header.php
+++ header.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LegacyEvents - Magiczne Wydarzenia</title>
+    <title>LegacyEvents - Tworzymy światy, nie tylko eventy - Eventy, Pokazy Świetlne, Videomapping</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800&family=Caveat:wght@600;700&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css?v=2">
+    <link rel="stylesheet" href="css/style.css?v=3">
     <?php
     if (isset($heroSliderImages) && is_array($heroSliderImages)) {
         foreach ($heroSliderImages as $imgUrl) {
